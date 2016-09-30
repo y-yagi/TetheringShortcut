5569a50317d4c3b5fc6c24315fc3aecfd2610bcf
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-09-30 20:25:29 +0900

    first commit

---
+++ app/src/main/java/io/github/y_yagi/tetheringshortcut/MainActivity.java
@@ -0,0 +1,22 @@
+package io.github.y_yagi.tetheringshortcut;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent();
+        intent.setClassName("com.android.settings", "com.android.settings.TetherSettings");
+        startActivity(intent);
+    }
+}
